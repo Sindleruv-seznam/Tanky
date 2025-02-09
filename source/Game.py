13f6a5d277b37cfd53ee80e08afa5a1e548e5f09
Matouš Šindler <sindlerm24@spstrutnov.cz>
2025-02-09 13:00:36 +0100

    Částečně funkční menu
    Částečně funkční menu bez textu, po stisknutí tlačítka se vypne aplikace, lehká úprava frameworku

--- source/Game.py
+++ source/Game.py
@@ -1,74 +1,105 @@
-# pouzite balicky
-import sys
-import math
+# pouzite balicky 
+import sys 
+import math 
+import random
+import pygame # pridani balicku (frameworku) Pygame 
+pygame.init() # priprava frameowrku k praci 
+ 
+# PRIPRAVA APLIKACE 
+ 
+ROZLISENI_OKNA_X = 800 
+ROZLISENI_OKNA_Y = 600 
+ 
+okno = pygame.display.set_mode((ROZLISENI_OKNA_X, ROZLISENI_OKNA_Y)) # vytvoreni okna pro vykreslovani 
+pygame.display.set_caption('Tanky') 
+ 
+casovac_FPS = pygame.time.Clock() 
+ 
+# priprava promennych 
+velikost_x = 200 
+velikost_y = 50 
 
-import pygame # pridani balicku (frameworku) Pygame
-pygame.init() # priprava frameowrku k praci
+stred_obrazovky = (ROZLISENI_OKNA_X//2) - velikost_x//2, (ROZLISENI_OKNA_Y//2) - velikost_y//2 
 
-# PRIPRAVA APLIKACE
+poloha_x = stred_obrazovky[0] 
+poloha_y = stred_obrazovky[1] 
+ 
+rychlost = 5 # pixely / frame 
+uhel = 30    # uhel pohybu ve stupnich 
 
-ROZLISENI_OKNA_X = 800
-ROZLISENI_OKNA_Y = 600
+font = pygame.font.Font(None, 32) 
 
-okno = pygame.display.set_mode((ROZLISENI_OKNA_X, ROZLISENI_OKNA_Y)) # vytvoreni okna pro vykreslovani
-pygame.display.set_caption('Pong')
+white = (255, 255, 255)
+grey = (200, 200, 200)
+red = (255, 0, 0)
+blue = (0, 0, 255)
 
-casovac_FPS = pygame.time.Clock()
+b_barva = [blue, blue, blue]
+b_active = [False, False, False]
 
-# priprava promennych
-velikost_x = 200
-velikost_y = 50
-
-stred_obrazovky = (ROZLISENI_OKNA_X//2) - velikost_x//2, ROZLISENI_OKNA_Y//2 - velikost_y//2
-poloha_x = stred_obrazovky[0]
-poloha_y = stred_obrazovky[1]
-
-rychlost = 5 # pixely / frame
-uhel = 30    # uhel pohybu ve stupnich
-
-font = pygame.font.Font(None, 50)
-
-# vykreslovaci smycka
-while True:
-    # OVLADANI APLIKACE
-    
-    # detekce udalosti v aplikaci
-    for udalost in pygame.event.get():
-        # detekce udalosti vypnuti aplikace (krizkem nebo ALT+F4)
-        if udalost.type == pygame.QUIT:
-            pygame.quit() # vypnuti frameworku
-            sys.exit()    # vypnuti cele aplikace
-        # detekce stisku klavesy Escape
-        if udalost.type == pygame.KEYDOWN:
-            if udalost.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-    
-    # VYKRESLOVANI APLIKACE
-    
-    okno.fill((0, 200, 250)) # prebarveni okna jednolitou barvou
-    
-    #pozice mysi
+# vykreslovaci smycka 
+while True: 
+    # OVLADANI APLIKACE 
+    stisknute_klavesy = pygame.key.get_pressed()
     pozice_mysi = pygame.mouse.get_pos()
-button_rect = pygame.Rect(poloha_x, poloha_y, velikost_x, velikost_y)
+    LMB_active = pygame.mouse.get_pressed()[0]
 
-# Check for mouse click
-for event in pygame.event.get():
-    if event.type == pygame.MOUSEBUTTONDOWN:  # When mouse button is pressed
-        if button_rect.collidepoint(pozice_mysi):  # If mouse is over button
-            print("Button was clicked!")
-            # You can add any action you want here
-            
-# Draw the button (red when hovered, gray normally)
-if button_rect.collidepoint(pozice_mysi):
-    pygame.draw.rect(okno, (255, 0, 0), button_rect)  # Red when hovered
-else:
-    pygame.draw.rect(okno, (128, 128, 128), button_rect)  # Gray normally
+    # detekce udalosti v aplikaci 
+    for udalost in pygame.event.get(): 
+        # detekce udalosti vypnuti aplikace (krizkem nebo ALT+F4) 
+        if udalost.type == pygame.QUIT: 
+            pygame.quit() # vypnuti frameworku 
+            sys.exit()    # vypnuti cele aplikace 
+        # detekce stisku klavesy Escape 
+        if udalost.type == pygame.KEYDOWN: 
+            if udalost.key == pygame.K_ESCAPE: 
+                pygame.quit() 
+                sys.exit() 
+
+    # VYKRESLOVANI APLIKACE 
+     
+    okno.fill((white)) # prebarveni okna jednolitou barvou 
 
     # menu
-    pygame.draw.rect(okno, (100, 100, 100), (poloha_x, (poloha_y), velikost_x, velikost_y))
+    def menu():
+        button1 = pygame.Rect(poloha_x, (poloha_y), velikost_x, velikost_y)
+        pygame.draw.rect(okno, b_barva[0], button1)
+        
+        if button1.collidepoint(pozice_mysi):
+            b_barva[0] = red
+        else:
+            b_barva[0] = blue
 
+        if button1.collidepoint(pozice_mysi) and LMB_active == True:
+            b_active[1] = True
 
-    pygame.display.update() # prehozeni framebufferu na displej
+        button2 = pygame.Rect(poloha_x, (poloha_y + 100), velikost_x, velikost_y)
+        pygame.draw.rect(okno, b_barva[1], button2)
+        
+        if button2.collidepoint(pozice_mysi):
+            b_barva[1] = red
+        else:
+            b_barva[1] = blue
+        
+        if button2.collidepoint(pozice_mysi) and LMB_active == True:
+            b_active[1] = True
+
+        button3 = pygame.Rect(poloha_x, (poloha_y - 100), velikost_x, velikost_y)
+        pygame.draw.rect(okno, b_barva[2], button3)
+        
+        if button3.collidepoint(pozice_mysi):
+            b_barva[2] = red
+        else:
+            b_barva[2] = blue
+        
+        if button3.collidepoint(pozice_mysi) and LMB_active == True:
+            b_active[2] = True
+
+    menu()
+    if b_active[0] or b_active[1] or b_active[2] == True:
+        pygame.quit()
+        sys.exit()
     
-    casovac_FPS.tick(60) # omezeni FPS
+    pygame.display.update() # prehozeni framebufferu na displej
+
+    casovac_FPS.tick(60) # omezeni FPS 
